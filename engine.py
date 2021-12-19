81605d39c7dc255ff1332c0be4ca0d9545e94e26
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-19 17:28:38 +1000

    Update engine.py

--- engine.py
+++ engine.py
@@ -91,6 +91,9 @@ def evaluate(data_loader, model, device):
         metric_logger.update(loss=loss.item())
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
         metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        
+        del samples, targets
+        torch.cuda.empty_cache()
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
