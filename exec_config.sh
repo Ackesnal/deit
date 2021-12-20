7486ea5bbb6bea6a1f43ee30beac7ad72bfe8e57
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-20 11:29:46 +1000

    Update exec_config.sh

--- exec_config.sh
+++ exec_config.sh
@@ -9,11 +9,4 @@
 #SBATCH -o modified3_new3_shuffle_deit_tiny_out.txt
 #SBATCH -e modified3_new3_shuffle_deit_tiny_err.txt
 
-
-"""
-for i in {1..36}; do
-    srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new3_shuffle_deit_tiny --resume ./output/modified3_new3_shuffle_deit_tiny/checkpoint.pth
-    wait
-done
-"""
 srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new4_shuffle_deit_tiny --resume ./output/modified3_new_shuffle_deit_tiny/checkpoint.pth
