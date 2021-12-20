4e154763740a5079e975a8e96486d0c0c48159db
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-20 11:52:10 +1000

    Update exec_config.sh

--- exec_config.sh
+++ exec_config.sh
@@ -3,10 +3,13 @@
 #SBATCH -n 1
 #SBATCH --job-name=Ackesnal
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:tesla-smx2:2
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:tesla-smx2:4
 #SBATCH --mem-per-cpu=10G
 #SBATCH -o modified_new4_shuffle_deit_tiny_out.txt
 #SBATCH -e modified_new4_shuffle_deit_tiny_err.txt
 
-srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new4_shuffle_deit_tiny --resume ./output/modified3_new_shuffle_deit_tiny/checkpoint.pth
+for i in {1..100}; do
+    srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new4_shuffle_deit_tiny --resume ./output/modified3_new_shuffle_deit_tiny/checkpoint.pth
+    wait
+done
