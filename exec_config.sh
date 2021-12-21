5dc08ca288b9b95d248e24127832d6c2cbf33704
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-21 16:34:08 +1000

    Update exec_config.sh

--- exec_config.sh
+++ exec_config.sh
@@ -6,10 +6,10 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:tesla-smx2:4
 #SBATCH --mem-per-cpu=10G
-#SBATCH -o modified_new4_shuffle_deit_tiny_out.txt
-#SBATCH -e modified_new4_shuffle_deit_tiny_err.txt
+#SBATCH -o modified_new5_shuffle_deit_tiny_out.txt
+#SBATCH -e modified_new5_shuffle_deit_tiny_err.txt
 
 for i in {1..100}; do
-    srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new4_shuffle_deit_tiny --resume ./output/modified3_new_shuffle_deit_tiny/checkpoint.pth
+    srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 512 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified_new5_shuffle_deit_tiny --resume ./output/modified_new4_shuffle_deit_tiny/checkpoint.pth
     wait
 done
