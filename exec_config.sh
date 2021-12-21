4090d413eeb2a764e416615c544576a8b55f002d
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-21 18:23:37 +1000

    Update exec_config.sh

--- exec_config.sh
+++ exec_config.sh
@@ -3,13 +3,11 @@
 #SBATCH -n 1
 #SBATCH --job-name=Ackesnal
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=20
-#SBATCH --gres=gpu:tesla-smx2:4
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:tesla-smx2:2
 #SBATCH --mem-per-cpu=10G
-#SBATCH -o modified_new5_shuffle_deit_tiny_out.txt
-#SBATCH -e modified_new5_shuffle_deit_tiny_err.txt
+#SBATCH -o modified_new6_shuffle_deit_tiny_out.txt
+#SBATCH -e modified_new6_shuffle_deit_tiny_err.txt
 
-for i in {1..100}; do
-    srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 512 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new5_shuffle_deit_tiny --resume ./output/modified3_new4_shuffle_deit_tiny/checkpoint.pth --min-lr 2e-5 --drop-path 0.01
-    wait
-done
+srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 1024 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/modified3_new6_shuffle_deit_tiny --resume ./output/modified3_new_shuffle_deit_tiny/checkpoint.pth --min-lr 2e-5 --drop-path 0.01
+
