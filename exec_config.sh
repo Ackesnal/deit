ad3bd3a0a636fed6e65ecc5d02d1a3c586a5e512
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2022-07-12 12:34:28 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -1,13 +1,14 @@
-BATCH -N 1
+#!/bin/bash
+#SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --job-name=Ackesnal
+#SBATCH --job-name=original
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=5G
-#SBATCH -o dense_predict_deit_tiny_out.txt
-#SBATCH -e dense_predict_deit_tiny_err.txt
+#SBATCH -o original_deit_tiny_out.txt
+#SBATCH -e original_deit_tiny_err.txt
 
-srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 512 --data-path ../data/imagenet/ --output_dir ./output/dense_predict_deit_tiny
+srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_patch16_224 --batch-size 512 --data-path ../data/imagenet/ --output_dir ./output/original_deit_tiny
 
 
