e3844e854a6fd6b589544a1b5143886401dee6a6
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2022-07-12 11:52:32 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -4,10 +4,10 @@
 #SBATCH --job-name=Ackesnal
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=20
-#SBATCH --gres=gpu:tesla-smx2:4
-#SBATCH --mem-per-cpu=10G
-#SBATCH -o normed_shuffle_deit_tiny_another_out.txt
-#SBATCH -e normed_shuffle_deit_tiny_another_err.txt
+#SBATCH --gres=gpu:2
+#SBATCH --mem-per-cpu=5G
+#SBATCH -o dense_predict_deit_tiny_out.txt
+#SBATCH -e dense_predict_deit_tiny_err.txt
 
-srun python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 2048 --data-path ../../uqxxu16/data/imagenet/ --output_dir ./output/normed_shuffle_deit_tiny_another --resume ./output/normed_shuffle_deit_tiny_another/checkpoint.pth --min-lr 1e-8 --drop-path 0.01 --epochs 400
+srun python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_tiny_shuffle_patch16_224 --batch-size 512 --data-path ../data/imagenet/ --output_dir ./output/dense_predict_deit_tiny
 
