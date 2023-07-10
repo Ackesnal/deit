d84a884846d9f5cee85940660789c8e3f4c82836
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2023-07-10 20:53:23 +1000

    updated

--- models_v3.py
+++ models_v3.py
@@ -25,31 +25,33 @@ class Mlp(nn.Module):
             in_features,
             hidden_features=None,
             out_features=None,
-            num_heads=8,
+            num_heads=3,
             act_layer=nn.GELU,
-            bias=True,
-            drop=0.,
-            use_conv=False,
-            
+            drop=0.
     ):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         
-        in_features = in_features 
-        out_features = out_features 
-        hidden_features = hidden_features 
+        in_features = in_features
+        out_features = out_features
+        hidden_features = hidden_features
+        self.num_heads = num_heads
         
-        self.fc1 = nn.Conv1d(in_features, hidden_features, 1, groups=num_heads)
+        self.fc1 = nn.Conv1d(in_channels=in_features, out_channels=hidden_features, kernel_size=1, groups=num_heads)
+        self.fc2 = nn.Conv1d(in_channels=hidden_features, out_channels=out_features, kernel_size=1, groups=num_heads)
         self.act = act_layer()
-        self.fc2 = nn.Conv1d(hidden_features, out_features, 1, groups=num_heads)
+        self.drop1 = nn.Dropout(drop)
+        self.drop2 = nn.Dropout(drop)
         
     def forward(self, x):
-        x = x.transpose(1,2)
+        x = x.transpose(1, 2) # B, C, N
         x = self.fc1(x)
         x = self.act(x)
+        x = self.drop1(x)
         x = self.fc2(x)
-        x = x.transpose(1,2)
+        x = self.drop2(x)
+        x = x.transpose(1, 2) # B, N, C
         return x
 
 
@@ -58,21 +60,32 @@ class Attention(nn.Module):
     def __init__(
             self,
             dim,
-            num_heads=8
+            num_heads=3,
+            attn_drop=0.,
+            proj_drop=0.
+            
     ):
         super().__init__()
         assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        
         self.num_heads = num_heads
-        self.head_dim = dim // num_heads
-        self.qkv = nn.Conv1d(dim, dim*3, 1, groups=num_heads)
-        self.proj = nn.Conv1d(dim, dim, 1, groups=num_heads)
+        self.attn_drop = attn_drop
+        self.qkv = nn.Conv1d(in_channels=dim, out_channels=dim*3, kernel_size=1, groups=num_heads)
+        self.proj = nn.Conv1d(in_channels=dim, out_channels=dim, kernel_size=1)
+        self.proj_drop = nn.Dropout(proj_drop)
 
     def forward(self, x):
         B, N, C = x.shape
         qkv = self.qkv(x.transpose(1,2)).reshape(B, 3, self.num_heads, C//self.num_heads, N).permute(1,0,2,4,3) # B, C*3, N
         q, k, v = torch.chunk(qkv, chunks=3, dim=0) # B, H, N, C/H
-        x = F.scaled_dot_product_attention(q, k, v) # B, H, N, C/H
-        x = self.proj(x.transpose(2,3).reshape(B, C, N)).transpose(1,2)
+        
+        # shuffle
+        v = v.reshape(B, self.num_heads, N, self.num_heads, C//self.num_heads//self.num_heads).transpose(1,3).reshape(B, self.num_heads, N, C//self.num_heads)
+        
+        x = F.scaled_dot_product_attention(q, k, v, dropout_p=self.attn_drop) # B, H, N, C/H
+        
+        x = self.proj(x.transpose(2,3).reshape(B, C, N)).transpose(1,2) # N
+        x = self.drop_proj(x)
         return x
         
 
@@ -95,20 +108,25 @@ class Block(nn.Module):
     ):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention(dim, num_heads=num_heads)
+        self.attn = Attention(dim = dim, 
+                              num_heads=num_heads, 
+                              attn_drop=0., 
+                              proj_drop=0.)
         self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
-
+        
         self.norm2 = norm_layer(dim)
-        self.mlp = mlp_layer(in_features=dim, hidden_features=int(dim * mlp_ratio), 
-                             num_heads=num_heads, act_layer=act_layer)
+        self.mlp = mlp_layer(in_features=dim, 
+                             hidden_features=int(dim * mlp_ratio), 
+                             out_features=dim,
+                             num_heads=num_heads,
+                             act_layer=act_layer)
         self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
-        self.num_heads = num_heads
-
+        
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        
     def forward(self, x):
-        x = x + self.ls1(self.attn(self.norm1(x)))
-        B, N, C = x.shape
-        x = x.reshape(B, N, self.num_heads, C//self.num_heads).transpose(-1,-2).reshape(B, N, C)
-        x = x + self.ls2(self.mlp(self.norm2(x)))
+        x = x + self.drop_path(self.ls1(self.attn(self.norm1(x))))
+        x = x + self.drop_path(self.ls2(self.mlp(self.norm2(x))))
         return x
 
         
@@ -244,8 +262,8 @@ def shufformer_tiny_224(pretrained=False,
                               mlp_ratio=4, qkv_bias=True,
                               norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     """
-    model = ShuffleTransformer(patch_size=16, embed_dim=256, depth=12,
-                               num_heads=2, mlp_ratio=4, qkv_bias=True,
+    model = ShuffleTransformer(patch_size=16, embed_dim=288, depth=12,
+                               num_heads=3, mlp_ratio=4, qkv_bias=True,
                                norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
     
