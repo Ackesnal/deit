21a24f5590ab9a6ccd0bc4d32a66616cec00162a
uqxxu16 <uqxxu16@wiener.hpc.dc.uq.edu.au>
2024-05-22 18:26:28 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -6,12 +6,12 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:tesla-smx2:1
 #SBATCH --mem-per-cpu=2G
-#SBATCH -o train_out.txt
-#SBATCH -e train_err.txt
+#SBATCH -o RePaViT_small_patch16_224_layer12_ChannelIdle_BS2048_lr2e-3_min-lr2e-5_warmup5epoch_gain0.1_ADAMW_weightdecay0.01_out.txt
+#SBATCH -e RePaViT_small_patch16_224_layer12_ChannelIdle_BS2048_lr2e-3_min-lr2e-5_warmup5epoch_gain0.1_ADAMW_weightdecay0.01_err.txt
 
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-export MASTER_PORT=12580
+export MASTER_PORT=12582
 export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
-export BATCH_SIZE=2048 / $WORLD_SIZE
+export BATCH_SIZE=$(echo "scale=0; 2048 / $WORLD_SIZE" | bc)
 
-srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env main.py --data-path /scratch/itee/uqxxu16/data/imagenet --batch-size=$BATCH_SIZE --model=RePaViT_small_patch16_224_layer12 --output_dir=output/debug --feature_norm=BatchNorm --shortcut_gain=0.2 --lr=5e-4 --min-lr=1e-6 --warmup-lr=1e-6 --opt=nadamw --num_workers=15 --weight-decay=0.05 --warmup-epochs=20 --channel_idle 
+srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env main.py --data-path /scratch/itee/uqxxu16/data/imagenet --batch-size=$BATCH_SIZE --model=RePaViT_small_patch16_224_layer12 --output_dir=output/RePaViT_small_patch16_224_layer12_ChannelIdle_BS2048_lr2e-3_min-lr2e-5_warmup5epoch_gain0.1_ADAMW_weightdecay0.01 --feature_norm=BatchNorm --shortcut_gain=0.1 --lr=1e-3 --min-lr=1e-5 --warmup-lr=1e-6 --opt=adamw --num_workers=15 --weight-decay=0.01 --warmup-epochs=5 --channel_idle
